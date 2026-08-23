0dedc5f1feb84b95f288cc94237f2aba1fd2cd07
Anurag Singh <128205525+Anuragsingh1751@users.noreply.github.com>
2026-08-23 22:35:53 +0530

    Time: 942 ms (42.33%), Space: 0B (100%) - LeetHub

---
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+delete p1
+FROM Person p1
+join person p2
+    on p1.email = p2.email
+    and p1.id>p2.id
+;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
